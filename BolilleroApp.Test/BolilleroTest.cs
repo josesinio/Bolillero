a684d4692e0d0f1fc65b15a8004e6b1a277e8590
09MauroID <jose.cruzet12d1@gmail.com>
2023-04-24 18:20:50 -0300

    update

--- BolilleroApp.Test/BolilleroTest.cs
+++ BolilleroApp.Test/BolilleroTest.cs
@@ -4,26 +4,22 @@ namespace BolilleroApp.Test;
 
 public class BolilleroTest
 {
-    public Bolillero Bolillero1{get; set;}
-    public BolilleroTest()=> Bolillero1 = new Bolillero(new Azar());
-    [Fact]
-    public void Constructor()
-    {
-        Assert.Equal(new List<int>{0,1,2,3,4,5,6,7,8,9},Bolillero1.Adentro);
+    public Bolillero Bolillero1 { get; set; }
+    public BolilleroTest() => Bolillero1 = new Bolillero(10, new Azar());
 
-    }
 
     [Fact]
     public void SacarBolillaTest()
     {
-        var numero =Bolillero1.SacarBolillas();
-        var esperado =0;
-        var esperado1 =9;
-        var esperado2 =1;
-        
+
+        var numero = Bolillero1.SacarBolillas();
+        var esperado = 0;
+        var esperado1 = 9;
+        var esperado2 = 1;
+
         Assert.Equal(numero, esperado);
         Assert.Equal(esperado1, Bolillero1.Adentro.Count);
-        Assert.Equal(esperado2,Bolillero1.Afuera.Count);
+        Assert.Equal(esperado2, Bolillero1.Afuera.Count);
     }
 
     [Fact]
@@ -31,23 +27,23 @@ public class BolilleroTest
     {
         Bolillero1.SacarBolillas();
         Bolillero1.ReingresarBolillas();
-        var esperado =10;
+        var esperado = 10;
         var esperado1 = 0;
 
-        Assert.Equal(esperado,Bolillero1.Adentro.Count);
+        Assert.Equal(esperado, Bolillero1.Adentro.Count);
         Assert.Equal(esperado1, Bolillero1.Afuera.Count);
     }
     [Fact]
     public void JugarGana()
     {
-        var intentoGana = Bolillero1.Jugar(Jugada: new List<int>(){0,1,2,3});
-        Assert.True( intentoGana);
+        var intentoGana = Bolillero1.Jugar(Jugada: new List<int>() { 0, 1, 2, 3 });
+        Assert.True(intentoGana);
     }
 
     [Fact]
     public void JugarPierde()
     {
-        var intentoPierde = Bolillero1.Jugar(Jugada: new List<int>{4,2,1});
+        var intentoPierde = Bolillero1.Jugar(Jugada: new List<int> { 4, 2, 1 });
 
         Assert.False(intentoPierde);
     }
@@ -55,8 +51,8 @@ public class BolilleroTest
     [Fact]
     public void JugarNVecesGana()
     {
-        var jugada = Bolillero1.JugarNVeces(Jugada: new List<int>{0,1}, 1);
+        var jugada = Bolillero1.JugarNVeces(Jugada: new List<int> { 0, 1 }, 1);
 
-        Assert.Equal(1,jugada);
+        Assert.Equal(1, jugada);
     }
 }
--- BolilleroApp.core/Bolillero.cs
+++ BolilleroApp.core/Bolillero.cs
@@ -5,18 +5,17 @@ public class Bolillero
     public List<int> Jugada { get; set; }
     public List<int> Afuera { get; set; }
     public IAzar Random { get; set; }
-    public Bolillero(IAzar random)
+    public Bolillero(int bolillas, IAzar random)
     {
-        this.Adentro = new List<int>();
+        this.Adentro = new List<int>(bolillas);
         this.Jugada = new List<int>();
         this.Afuera = new List<int>();
-        Random= random;
+        Random = random;
     }
     public int SacarBolillas()
     {
         var indice = Random.SacarIndice(this.Adentro);
         var elemento = Adentro[indice];
-
 
         this.Afuera.Add(elemento);
         this.Adentro.RemoveAt(indice);
@@ -42,23 +41,21 @@ public class Bolillero
     public int JugarNVeces(List<int> Jugada, int cantidad)
     {
         var Victoria = 0;
-        for(int i =0; i <= cantidad; i++)
+        for (int i = 0; i <= cantidad; i++)
         {
             var intento = Jugar(Jugada);
 
-            if(intento== true)
+            if (intento == true)
             {
-                Victoria=+1;
+                Victoria = +1;
             }
         }
         return Victoria;
     }
     public void ReingresarBolillas()
     {
-        var elemento1 = SacarBolillas();
-
-        Adentro.Add(elemento1);
-        Afuera.Remove(elemento1);
+        Adentro.AddRange(Afuera);
+        Afuera.Clear();
     }
 
 }
